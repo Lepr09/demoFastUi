1b27fc2df585b882d4caebce2a0ed44ec4e3b932
Lyubov Danilova <sniper@Lyubovs-Air.fritz.box>
2022-02-21 23:05:01 +0300

    first

---
+++ specs/auth.spec.mjs
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Vikunja Authorization', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://try.vikunja.io/');
+    });
+
+    test('demo user can login', async ({ page }) => {
+        await page.click('#username')
+        await page.type('#username', 'demo')
+        await page.click('#password')
+        await page.type('#password', 'demo')
+        await page.click('.content > div > div > #loginform > .base-button')
+        expect(await page.textContent('.user > .dropdown > .dropdown-trigger > .base-button > .username')).toBe('demo');
+    });
+});
+
+
+
